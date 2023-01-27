b2f73b32675293d0c07e549c88d85aa215e8c1ed
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-01-26 19:09:04 -0500

    se crea migrador

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportExportController;
+use Illuminate\Support\Facades\Artisan;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +16,10 @@ use App\Http\Controllers\ImportExportController;
 
 Route::get('/', function () {
     return view('Layout.layout');
+});
+
+Route::get('/configuration', function () {
+    Artisan::call('migrate');
 });
 
 Route::get('import/documents', [ImportExportController::class, 'index']);
