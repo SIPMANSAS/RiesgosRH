8c7e485d04c49eec2ecff22ce2b212befa01b9a4
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-01-30 16:33:55 -0500

    mejora en dominio

--- resources/views/Dashboard/AdminDominios/AdminDominiosGrupo/index.blade.php
+++ resources/views/Dashboard/AdminDominios/AdminDominiosGrupo/index.blade.php
@@ -19,8 +19,12 @@
 @endpush
 @section('barra-superior')
     <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-        <h2 class="text-white pb-2 fw-bold">Administrar Dominios</h2>
-        <a href="{{ url('Administrar/dominios') }}" class="btn btn danger">Volver</a>
+        <div>
+            <h2 class="text-white pb-2 fw-bold">Administrar Dominios</h2>
+        </div>
+        <div class="ml-md-auto py-2 py-md-0">
+            <a href="{{ url('Administrar/dominios') }}" class="btn btn-danger btn-round">Volver</a>
+        </div>
     </div>
 @endsection
 @section('contenido')
