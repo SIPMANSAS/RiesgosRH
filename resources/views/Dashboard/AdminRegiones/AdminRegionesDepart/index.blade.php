5a0de3b4c7e5ab0fd33317bb470a3e84c206ed99
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-01-30 16:26:09 -0500

    mejora barra superior en Regiones

--- resources/views/Dashboard/AdminRegiones/AdminRegionesDepart/index.blade.php
+++ resources/views/Dashboard/AdminRegiones/AdminRegionesDepart/index.blade.php
@@ -20,22 +20,12 @@
 @section('barra-superior')
 <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
     <div>
-        <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-        <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
+        <h2 class="text-white pb-2 fw-bold">Administrar Regiones</h2>
     </div>
     <div class="ml-md-auto py-2 py-md-0">
-        <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
-        <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
+        <a href="{{ url('Administrar/regiones') }}" class="btn btn-danger">Volver</a>
     </div>
 </div>
-
-        <!---
-        <div>
-            <h2 class="text-white pb-2 fw-bold">Administrar Regiones</h2>
-        </div>
-        <div class="ml-md-auto py-2 py-md-0">
-            <a href="{{ url('Administrar/regiones') }}" class="btn btn-danger">Volver</a>
-        </div>--->
 @endsection
 @section('contenido')
     <div class="page-inner mt--5">
