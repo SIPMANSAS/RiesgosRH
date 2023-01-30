c39e332104af944b20198ee4b35d0538493e4d8a
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-01-30 16:24:54 -0500

    mejora barra superior

--- resources/views/Dashboard/AdminRegiones/AdminRegionesDepart/index.blade.php
+++ resources/views/Dashboard/AdminRegiones/AdminRegionesDepart/index.blade.php
@@ -18,17 +18,17 @@
     </script>
 @endpush
 @section('barra-superior')
-    <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-            <div>
-                <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-                <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
-            </div>
-            <div class="ml-md-auto py-2 py-md-0">
-                <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
-                <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
-            </div>
-        </div>
+<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+    <div>
+        <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
+        <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
+    </div>
+    <div class="ml-md-auto py-2 py-md-0">
+        <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
+        <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
+    </div>
+</div>
+
         <!---
         <div>
             <h2 class="text-white pb-2 fw-bold">Administrar Regiones</h2>
@@ -36,7 +36,6 @@
         <div class="ml-md-auto py-2 py-md-0">
             <a href="{{ url('Administrar/regiones') }}" class="btn btn-danger">Volver</a>
         </div>--->
-    </div>
 @endsection
 @section('contenido')
     <div class="page-inner mt--5">
