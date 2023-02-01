7a7bea638941fa81a635bbe75409925678437d09
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-01-31 20:16:51 -0500

    update

--- app/Http/Controllers/TerceroController.php
+++ app/Http/Controllers/TerceroController.php
@@ -27,6 +27,7 @@ class TerceroController extends Controller
     public function create()
     {
         $TipoIdentificacion = DB::select("CALL p_dominio(18)");
+        dd($TipoIdentificacion);
         return view('Dashboard.Terceros.create',compact('TipoIdentificacion'));
     }
 
